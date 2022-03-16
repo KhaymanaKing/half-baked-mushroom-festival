2df6720aaae0abba6605356b4402264aead5395d
Khayman King <khaymanak@gmail.com>
2022-03-16 14:49:04 -0700

    generates mushrooms and people

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ addMushroomButton.addEventListener('click', () => {
 
 addFriendButton.addEventListener('click', () => {
     // get the name from the input
+    
     // create a new friend object
     // push it into the friends state array, passed in as an argument
     // reset the input
@@ -54,12 +55,23 @@ function displayFriends() {
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
-
+        const friendEl = renderFriend(friend);
         // this is a clickable list, so . . .
         //     add an event listener to each friend
+        friendEl.addEventListener('click', () =>{
+            if (friend.satisfaction < 3 && mushroomCount > 0){
         //         and if the friend's satisfaction level is below 3 and you have mushrooms left
         //             increment the friends satisfaction and decrement your mushrooms
         //             then display your friends and mushrooms with the updated state
+                friend.satisfaction++;
+                mushroomCount--;
+
+                displayFriends();
+                displayMushrooms();
+            }
+        });
+
+        friendsEl.append(friendEl);
 
         // append the friendEl to the friends list in DOM
     }
@@ -67,9 +79,12 @@ function displayFriends() {
 
 function displayMushrooms() {
     // clear out the mushroom div
-
+    mushroomsEl.textConter = '';
     for (let i = 0; i < mushroomCount; i++) {
         // for each mushroom in your mushroom state, render and append a mushroom
+        const mushroomEl = renderMushroom();
+
+        mushroomsEl.append(mushroomEl);
     }
 }
 
